e0c95c6aff37965995aa12343271996c0de24b03
ab-rahman <abr03348@gmail.com>
2025-04-17 18:20:19 +0600

    implemented user update, delete and changing role from admin dashboard

--- app/(dashboards)/admin-dashboard/users/page.tsx
+++ app/(dashboards)/admin-dashboard/users/page.tsx
@@ -1,12 +1,17 @@
-import { UserManagement } from "../components/user-management"
-import { AdminHeader } from "../components/admin-header"
-import { AdminShell } from "../components/admin-shell"
+"use client";
+
+import { UserManagement } from "../components/user-management";
+import { AdminHeader } from "../components/admin-header";
+import AdminShell from "../components/admin-shell";
 
 export default function UsersPage() {
   return (
     <AdminShell>
-      <AdminHeader heading="User Management" text="Manage and monitor user accounts."></AdminHeader>
+      <AdminHeader
+        heading="User Management"
+        text="Manage and monitor user accounts."
+      ></AdminHeader>
       <UserManagement />
     </AdminShell>
-  )
+  );
 }
